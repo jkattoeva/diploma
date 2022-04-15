3bbb18d544d92d8b23fabb94cffb17ded91cfa45
Janetta Kataoeva <jkattoeva@gmail.com>
2022-04-15 20:29:44 +0600

    changed navigation and added cards

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
 import "./Header.css";
 import headerImage from "../../assets/shoe5.jpg";
+import logo from "../../assets/shop.jpg";
 function Header() {
   const style = {
     backgroundImage: `url(${headerImage})`
   };
 
+
   return (
     <header className="Header" style={style}>
+      <div className="lg">
       <h1> <span className="first">The best</span> <br/> <span className="second">online store of </span><br/><span className="third">shoe collections in KG</span></h1>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      </div>
     </header>
   );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,41 @@
 import "./Main.css";
-function Main () {
-  return (
-    <h1>HELLO WORLD</h1>
-  );
-};
+
+function Main() {
+  const cards = [
+    {
+      id: 1,
+      text: "lorem lorem lorm dihdc frhi x",
+      title: "ZARA",
+      url: "https://hobokengirl.com/wp-content/uploads/2016/10/zara-logo1.jpg",
+    },
+    {
+      id: 2,
+      title: "accusamus beatae ad facilis cum similique qui sunt",
+      text: "accusamus beatae ad facilis cum similique qui sunt",
+      url: "https://hobokengirl.com/wp-content/uploads/2016/10/zara-logo1.jpg",
+    },
+    {
+      id: 3,
+      title: "accusamus beatae ad facilis cum similique qui sunt",
+      url: "https://hobokengirl.com/wp-content/uploads/2016/10/zara-logo1.jpg",
+      text: "accusamus beatae ad facilis cum similique qui sunt",
+    },
+  ];
+
+  const card = cards.map((item) => {
+    return (
+      <div className="Card">
+        <div className="Elements">
+          <div >
+            <img className="image" src={item.url} alt="" />
+          </div>
+          <h2>{item.title}</h2>
+          <p>{item.text} </p>
+        </div>
+      </div>
+    );
+  });
+  return <div className="Main">{card}</div>;
+}
 
 export default Main;
