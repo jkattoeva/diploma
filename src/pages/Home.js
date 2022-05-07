c1b86df48779e8dbc5bb331ac3e2fa79837adeac
Janetta Kataoeva <jkattoeva@gmail.com>
2022-05-08 00:29:27 +0600

    changed text  in header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <>
     <Header title="Welcome to our shoes store"
      image={headerImage}>
-    A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with form originally being tied to function. Though the human foot can adapt to varied terrains and climate conditions, it is still vulnerable to environmental hazards such as sharp rocks and temperature extremes, which shoes protect against. Some shoes are worn as safety equipment, such as steel-toe boots which are required footwear at industrial worksites.
+    A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture.
     </Header>
     </>
   );
