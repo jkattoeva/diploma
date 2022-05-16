3147f1bb2cc235863e482721bdad947eac8efd74
Janetta Kataoeva <jkattoeva@gmail.com>
2022-05-16 15:25:08 +0600

    changed navToggle components and delete some mistakes

--- src/components/Nav/NavToggle/navToggle.js
+++ src/components/Nav/NavToggle/navToggle.js
@@ -1,8 +1,7 @@
 import classes from "./NavToggle.module.css";
 
-
-function NavToggle({callback }) {
-  return(
+function NavToggle({ callback }) {
+  return (
     <button onClick={callback} className={classes.NavToggle}>
       &nbsp;
     </button>
