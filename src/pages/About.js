c6710dd6ac31e92c960cd9a5dc54d1fa2e875a17
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-20 13:19:05 +0600

    Correct problems

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,25 +5,12 @@ import headerImage from "../assets/headerImage2.jpg";
 function About() {
   return (
     <article>
-         <>
-    <Header title="We are the best shoes store in KG"
-     image={headerImage}>
-    Additionally, fashion has often dictated many design elements, such as whether shoes have very high heels or flat ones. Contemporary footwear varies widely in style, complexity and cost. 
-    </Header>
-    </>
-      <div className="List">
-      <ul>
-        <li>High quality</li>
-        <li>Max comfort</li>
-        <li>Extraordinary style</li>
-      </ul>
-      <ul>
-        <li>More lightness</li>
-        <li>Big choices</li>
-        <li>Low price</li>
-      </ul>
-      </div>
+      <Header title="We are the best shoes store in KG" image={headerImage}>
+        Additionally, fashion has often dictated many design elements, such as
+        whether shoes have very high heels or flat ones. Contemporary footwear
+        varies widely in style, complexity and cost.
+      </Header>
     </article>
   );
 }
- export default About;
+export default About;
