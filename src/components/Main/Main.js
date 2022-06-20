daf4c6910603c64d32f1c40b0b7cbb7013804f93
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-20 17:46:57 +0600

    Commented all Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,39 +1,39 @@
-import "./Main.css";
+// import "./Main.css";
 
-function Main({ image }) {
-  return (
-    <ul className="press">
-      <li
-        style={{
-          background: `url(${image})`,
-          backgroundPosition: 0,
-          backgroundSize: "cover"
-        }}
-        className="press-item "
-      ></li>
-      <li
-        style={{
-          background: `url(${image})`,
-          backgroundPosition: "-135px",
-          backgroundSize: "cover"
-        }}
-        className="press-item"
-      ></li>
-      <li style={{
-         background: `url(${image})`,
-         backgroundPosition: "-270px",
-         backgroundSize: "cover"
+// function Main({ image }) {
+//   return (
+//     <ul className="press">
+//       <li
+//         style={{
+//           background: `url(${image})`,
+//           backgroundPosition: 0,
+//           backgroundSize: "cover"
+//         }}
+//         className="press-item "
+//       ></li>
+//       <li
+//         style={{
+//           background: `url(${image})`,
+//           backgroundPosition: "-135px",
+//           backgroundSize: "cover"
+//         }}
+//         className="press-item"
+//       ></li>
+//       <li style={{
+//          background: `url(${image})`,
+//          backgroundPosition: "-270px",
+//          backgroundSize: "cover"
 
-          }}
-           className="press-item"></li>
-      <li style={{
-         background: `url(${image})` ,
-         backgroundPosition: "-405px",
-         backgroundSize: "cover"
-         }} 
-         className="press-item"></li>
-    </ul>
-  );
-}
+//           }}
+//            className="press-item"></li>
+//       <li style={{
+//          background: `url(${image})` ,
+//          backgroundPosition: "-405px",
+//          backgroundSize: "cover"
+//          }} 
+//          className="press-item"></li>
+//     </ul>
+//   );
+// }
 
-export default Main;
+// export default Main;
