b105ed2e63a20226c498cea20095be3d83eafcaa
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-24 15:11:15 +0600

    Added optimization in cartButton

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,11 +1,12 @@
 import { useDispatch } from "react-redux";
+import { add } from "../../redux/сartSlice";
 import classes from "./CartButton.module.css";
 
 export default function CartButton({ productId }) {
   const dispatch = useDispatch();
 
   function onAddToCart() {
-    dispatch({ type: 'cart/add', payload: productId });
+    dispatch(add(productId));
   }
 
   return (
