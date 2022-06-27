79cda436e7c87d1af6d8c1deae590f54bfb65ac5
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-27 15:10:06 +0600

    import authSlice in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 import сartSlice from "./сartSlice";
 
 
 const store = configureStore({
   reducer: {
     cart: сartSlice,
+    auth: authSlice,
   }
 });
 
