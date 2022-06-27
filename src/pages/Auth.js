1d59edfbb196244f0cfd66ea117c80f693965acd
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-27 15:08:47 +0600

    Added Authenticatication in Auth page and added function onSubmit for input and post in firebase

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,36 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { start } from "../redux/authSlice";
+
 export default function Auth() {
+  const dispatch = useDispatch();
+
+  function onAuthStart(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    dispatch(start({
+      email: formData.get('email'),
+      password: formData.get('password'),
+      method: 'signup'
+    }));
+  }
+
   return (
-    <form>
-    <label>
-      Email:
-      <input type="email" name="email" />
-    </label>
-    <label>
-      Password:
-      <input type="password" name="password" />
-    </label> 
-    
-    <button>Sign in</button>
-      <button>Sign up</button>
-    </form>
+    <div>
+      <form onSubmit={onAuthStart}>
+          <label>
+              Email:
+              <input type="email" name="email" />
+          </label>
+          <label>
+              Password:
+              <input type="password" name="password" />
+          </label>
+
+          <button>Sign in</button>
+          <button>Sign up</button>
+      </form>
+    </div>
   );
 }
