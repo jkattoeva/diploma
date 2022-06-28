f42a3b56ab496c235146a6c24ed54eb0556d2f85
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-29 01:59:23 +0600

    Delete all function and elements and imported new components  CartDisplay

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,53 +1,9 @@
-import Header from "../components/Header/Header";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../data/products";
-import { Link } from "react-router-dom";
-import classes from "../components/PagesStyles/Cart.module.css";
-import { decrement, increment, remove } from "../redux/сartSlice";
-
+import CartDisplay from "../components/CartDisplay/CartDisplay";
 
 function Cart() {
-  const items = useSelector((store) => store.cart.items);
-  const dispatch = useDispatch();
-  const products = getProducts();
-
-  let total = 0;
-  let output = products
-    .filter((product) => items[product.productId])
-    .map((product) => {
-      total += product.price * items[product.productId];
-
-      return (
-        <div className={classes.CartList}>
-          <img src={product.image} alt={product.title} className={classes.image} />
-          <Link className={classes.a} to="">{product.title}</Link> 
-          {items[product.productId]}
-          
-          <button onClick={() => dispatch(decrement(product.productId))}>-</button>
-          <div className={classes.prices}> $
-          {product.price * items[product.productId]}
-          </div>
-          <button onClick={() => dispatch(increment(product.productId))}>+</button>
-          <button onClick={() => dispatch(remove(product.productId))}>Delete</button>
-        </div>
-      );
-    });
-
-  if (!output.length) {
-    output = "No items in the cart.";
-  }
-
   return (
     <>
-      <Header title="Your Shopping Cart">
-        Please review items in your cart.
-      </Header>
-      <div>
-        {output}
-        <hr />
-        <div className={classes.total}>Total: ${total}</div>
-        <Link className={classes.checkout} to="/checkout"><button className={classes.buttons}>CheckOut</button></Link>
-      </div>
+    <CartDisplay />
     </>
   );
 }
