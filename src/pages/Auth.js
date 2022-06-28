e91f25117d80c53a0a8275b2f88321cea27b5e7e
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-28 12:57:42 +0600

    Added navigate for auth page and import useNavigate

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,9 +1,10 @@
-import React from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { start } from "../redux/authSlice";
 
 export default function Auth() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function onAuthStart(event) {
     event.preventDefault();
@@ -12,25 +13,25 @@ export default function Auth() {
     dispatch(start({
       email: formData.get('email'),
       password: formData.get('password'),
-      method: 'signup'
+      method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
     }));
+
+    navigate('/');
   }
 
   return (
-    <div>
-      <form onSubmit={onAuthStart}>
-          <label>
-              Email:
-              <input type="email" name="email" />
-          </label>
-          <label>
-              Password:
-              <input type="password" name="password" />
-          </label>
+    <form onSubmit={onAuthStart}>
+      <label>
+        Email:
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" />
+      </label>
 
-          <button>Sign in</button>
-          <button>Sign up</button>
-      </form>
-    </div>
+      <button>Sign in</button>
+      <button>Sign up</button>
+    </form>
   );
 }
