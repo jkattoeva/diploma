b16cb95ccdeb7d348cdabfc41f6d9175c91b1393
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-28 12:57:04 +0600

    added logic function for sign up and sign in

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,18 +3,27 @@ import NavItem from "./NavItem/NavItem";
 import Search from "../../assets/Profile.svg";
 import Favourite from "../../assets/favourite.svg";
 import CartLink from "../CartLink.js/CartLink";
+import { useSelector } from "react-redux";
 
-function Nav () {
+function Nav() {
+  const isAuthenticated = useSelector((store) => store.auth.idToken !== null);
+  
   return (
-      <ul className={classes.Nav}>
-        <NavItem url="/">Home</NavItem>
-        <NavItem url="/products">Products</NavItem>
-        <NavItem url="/about">About</NavItem>
-        <NavItem url="/signup"><img src={Search} alt="search"/></NavItem>
-        <CartLink />
-        <NavItem url="/favourite"><img src={Favourite} alt="favourite"/></NavItem>
-     </ul>
+    <ul className={classes.Nav}>
+      <NavItem url="/">Home</NavItem>
+      <NavItem url="/products">Products</NavItem>
+      <NavItem url="/about">About</NavItem>
+      <NavItem url="/signup">
+        <img src={Search} alt="search" />
+      </NavItem>
+      <CartLink />
+      <NavItem url="/favourite">
+        <img src={Favourite} alt="favourite" />
+      </NavItem>
+      {isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null}
+      {!isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null}
+    </ul>
   );
-};
+}
 
 export default Nav;
