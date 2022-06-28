dacbc318398da93b1f307d7f2bd4086c0150480b
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-29 01:58:37 +0600

    Added sign in and sign up logic function in websites navigate

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,25 @@
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
-import Search from "../../assets/Profile.svg";
+import signin from "../../assets/Profile.svg";
+import signout from "../../assets/signout.svg";
 import Favourite from "../../assets/favourite.svg";
 import CartLink from "../CartLink.js/CartLink";
 import { useSelector } from "react-redux";
 
 function Nav() {
   const isAuthenticated = useSelector((store) => store.auth.idToken !== null);
-  
+
   return (
     <ul className={classes.Nav}>
       <NavItem url="/">Home</NavItem>
       <NavItem url="/products">Products</NavItem>
       <NavItem url="/about">About</NavItem>
-      <NavItem url="/signup">
-        <img src={Search} alt="search" />
-      </NavItem>
       <CartLink />
       <NavItem url="/favourite">
         <img src={Favourite} alt="favourite" />
       </NavItem>
-      {isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null}
-      {!isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null}
+      {isAuthenticated ? <NavItem url="/signout"><img src={signout} alt="signin" /></NavItem> : null}
+      {!isAuthenticated ? <NavItem url="/auth"><img src={signin} alt="signout" /></NavItem> : null}
     </ul>
   );
 }
