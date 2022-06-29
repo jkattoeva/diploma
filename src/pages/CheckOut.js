3c696e4f6c39580d13b68d49faa685e0c07d1e3e
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-29 14:58:12 +0600

    Delete logic function in Checkout and import ChecoutDisplay component

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,65 +1,9 @@
-import Header from "../components/Header/Header";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { checkout } from "../redux/сartSlice";
+import CheckoutDisplay from "../components/CheckOutDisplay/CheckoutDispay";
 
-function Checkout() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { items, localId } = useSelector(store => ({
-    items: store.cart.items,
-    localId: store.auth.localId
-  }));
-
-  useEffect(() => {
-    if (!localId) {
-      navigate('/auth');
-    }
-  }, [localId, navigate]);
-
-  function onCheckout(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    dispatch(checkout({
-      localId: localId,
-      items: items,
-      ...Object.fromEntries(formData.entries()),
-    }));
-    navigate('/');
-  }
-
+function CheckOut() {
   return (
-    <>
-      <Header
-        title="Checkout">
-        Please enter your information.
-      </Header>
-
-
-      <form onSubmit={onCheckout}>
-        <label>
-          First name:
-          <input type="text" name="firstName" required />
-        </label>
-        <label>
-          Last name:
-          <input type="text" name="lastName" required />
-        </label>
-        <label>
-          Address:
-          <input type="text" name="address" required />
-        </label>
-        <label>
-          Phone:
-          <input type="text" name="phone" required />
-        </label>
-
-        <button>Complete the order</button>
-      </form>
-    </>
+    <CheckoutDisplay />
   );
 }
 
-export default Checkout;
+export default CheckOut;
