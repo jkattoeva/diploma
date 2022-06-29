ae180fb072eb478cbb85d67f078235e3dcb82545
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-29 14:55:25 +0600

    Added list for footer page and import module css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,30 @@
-import "./Footer.css";
+import classes from "./Footer.module.css";
+import facebook from "../../assets/facebook.svg";
+import instagram from "../../assets/instagram.svg";
+import pinterest from "../../assets/pinterest.svg";
+import twitter from "../../assets/twitter.svg";
 
 
 function Footer() {
   return(
-    <footer className="Footer">
+    <footer className={classes.Footer}>
       <div className="container">
-      Shoes store @2022
+          <ul className={classes.icons}>
+            <li><img src={facebook} alt="facebook"/></li>
+            <li><img src={instagram} alt="facebook"/></li>
+            <li><img src={pinterest} alt="facebook"/></li>
+            <li><img src={twitter} alt="facebook"/></li>
+          </ul>
+         <div> <ul className={classes.section}>
+            <li>Contact</li>
+            <li>FAQs</li>
+            <li>Privacy</li>
+            <li>Terms</li>
+            <li>Get Products</li>
+            <li>About Us</li>
+          </ul>
+          </div>
+          <p>@2022 FARFETCH All right reserved</p>
       </div>
     </footer>
   );
