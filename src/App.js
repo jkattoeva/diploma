ec3fa942ad77a030a7c38ead7dddba9c859fe1b9
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-30 13:30:25 +0600

    Added new route History

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { useDispatch } from "react-redux";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 import SignOut from "./pages/SignOut";
+import History from "./pages/History";
 
 function App() {
   const dispatch = useDispatch();
@@ -39,6 +40,7 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/signout" element={<SignOut />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/history" element={<History />} />
         </Routes>
       </Layout>
     </div>
