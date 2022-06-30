eb8863c46b07c4809b2857f2bdd1803128c894c6
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-30 13:37:01 +0600

    Added category header for category pages

---
+++ src/components/CategoryDisplay/CategoryDisplay.js
@@ -0,0 +1,16 @@
+import productImage from "../../assets/product1.jpg";
+import classes from "./CategoryDisplay.module.css";
+function CategoryDisplay() {
+  return (
+    <div className={classes.CategoryDisplay}>
+      <div>
+        {" "}
+        <h1>Our products are relevant for everyone</h1>
+        <p>You can order and get on and quality footwear products</p>
+      </div>
+      <img src={productImage} alt="image" />
+    </div>
+  );
+}
+
+export default CategoryDisplay;
